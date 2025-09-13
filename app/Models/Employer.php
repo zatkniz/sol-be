70d4a371aed6d7151fe2bb7d7ab02f9cb6e91862
stkitziris <stkitziris@gmail.com>
2025-09-13 18:11:27 +0300

    add soft deletes

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Employer extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'first_name',
@@ -18,6 +19,8 @@ class Employer extends Model
         'color',
         'order'
     ];
+
+    protected $dates = ['deleted_at'];
 
     public function bookings()
     {
